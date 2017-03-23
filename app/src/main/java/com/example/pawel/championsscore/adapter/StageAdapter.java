a05a228d52e6380dbff94c48517301fc5e533c76
Mateusz <lassaa785@gmail.com>
2017-03-23 20:19:15 +0100

    Adapter2

---
+++ app/src/main/java/com/example/pawel/championsscore/adapter/StageAdapter.java
@@ -0,0 +1,49 @@
+package com.example.pawel.championsscore.adapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.pawel.championsscore.R;
+import com.example.pawel.championsscore.model.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Mateusz on 23.03.2017.
+ */
+
+public class StageAdapter extends ArrayAdapter<Stage> {
+    private Context context;
+    @SuppressWarnings("unused")
+    private List<Stage> stages;
+
+    private List<Button> buttons = new ArrayList<>();
+
+    public StageAdapter(Context context, int resource, <Stage> objects) {
+        super(context, resource, objects);
+        this.context = context;
+        this.stages = objects;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+
+        LayoutInflater inflater =
+                (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.activity_stage, parent, false);
+
+        final Stage stage = stages.get(position);
+
+        TextView textView = (TextView) view.findViewById(R.id.textView);
+        textView.setText(stage.getName());
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/pawel/championsscore/model/Stage.java
@@ -0,0 +1,22 @@
+package com.example.pawel.championsscore.model;
+
+/**
+ * Created by Mateusz on 23.03.2017.
+ */
+
+public class Stage{
+
+private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Stage(String name){
+        this.name = name;
+    }
+}
